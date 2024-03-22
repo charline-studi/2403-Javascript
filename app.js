597aacf4b021456d60aa31160618e30ad31e6852
charline-studi <studi@charline-laporte.fr>
2024-03-22 11:09:36 +0100

    Ajout du cours #2

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { Search } from './modules/Search.js'
 
 // Entrée de mon programme
-console.log('Je suis bien dans App')
 new Search()
--- modules/Search.js
+++ modules/Search.js
@@ -1,6 +1,6 @@
 /* Ma bare de recherche : 
-- Récupérer la ville renseignée par mon visiteur
-- À partir du nom va aller trouver la latitude 
+- [x] : Récupérer la ville renseignée par mon visiteur
+- [ ] : À partir du nom va aller trouver la latitude 
 et la longitude correspondantes
 - BONUS : Si la ville renseignée n'est pas dans la base de données
 (json) elle doit indiquer l'erreur au visiteur
@@ -11,17 +11,63 @@ et la longitude correspondantes
 class Search {
 
     constructor() {
-        console.log('Je suis dans le constructeur')
         /* 1er temps = il sert à définir les variables, les propriétés */
-        this.input = 'my input'
+        this.input = document.querySelector('.js-search-input')
+        this.form = document.querySelector('.js-search-form')
+        this.cities = []
         /* 2nd temps = lancer les fonctions, les méthodes */
         this.init()
     }
 
     init() {
         /* Méthode pour lancer toutes les fonction de ma classe */
-        console.log('Je suis dans l\'init')
-        console.log(this.input)
+        this.getCities()
+        this.watchUserInput()
+    }
+
+    watchUserInput() {
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault()
+            this.getLatLong()
+        })
+    } 
+
+    getLatLong() {
+        const name = this.input.value
+        const cityData = this.getCityData(name)
+        if (cityData) {
+            const lat = cityData.lat
+            const long = cityData.lng
+        } else {
+            alert('La ville rensiengée n\'exsite pas')
+        }
+    }
+
+    getCities() {
+        fetch('../data/france-cities.json')
+            .then(response => response.json())
+            .then(data => {
+                this.cities = data
+            })
+    }
+
+    getCityDataWithFor(cityName) {
+        const cityNameLower = cityName.toLowerCase()
+        let cityData = {}
+        for (let i = 0; i < this.cities.length; i++) {
+            const cityNameInDataLower = this.cities[i].city.toLowerCase()
+            if (cityNameInDataLower === cityNameLower) {
+                cityData = this.cities[i]
+                break
+            }
+        }
+        return cityData
+    }
+
+    getCityData(userCityName) {
+        const userCityNameLower = userCityName.toLowerCase()
+        const data = this.cities.find( cityObject => cityObject.city.toLowerCase() === userCityNameLower)
+        return data
     }
 }
 
